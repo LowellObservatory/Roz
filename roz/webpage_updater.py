b83d5c957a85548d6204664976e3c1217e936c9c
Timothy P. Ellsworth Bowers <tbowers@lowell.edu>
2021-10-08 15:23:19 -0700

    Confluence webpage updater
    	modified:   roz/webpage_updater.py

--- roz/webpage_updater.py
+++ roz/webpage_updater.py
@@ -10,11 +10,106 @@
 
 """Update the Confluence (or other) Webpage with Latest Stats
 
+Confluence API Documentation: https://atlassian-python-api.readthedocs.io/index.html
+
 Further description.
 """
 
-def function1():
-    pass
+# Built-In Libraries
+import os
+import warnings
+
+# 3rd Party Libraries
+from astropy.table import Table
+from atlassian import Confluence
+import keyring
+
+# Internal Imports
+
+
+def test_confluence():
+    """test_confluence Dummy test routine
+
+    [extended_summary]
+    """
+    # Instantiate the class
+    confluence = setup_confluence()
+
+    # This the page we want to muck with
+    space = 'LDTOI'
+    title = 'LMI Filter Characterization 2'
+
+    # If it doesn't already exist, create it, then get the page_id
+    if not confluence.page_exists(space, title):
+        body = "Junk content added by python script."
+        confluence.create_page(space, title, body, parent_id=55214493)
+    page_id = confluence.get_page_id(space, title)
+
+    # Add a comment!
+    text = 'This comment was added via the atlassian-python-api'
+    confluence.add_comment(page_id, text)
+
+    # Make a CSV file to add to this page
+    t = Table.read('/Users/tbowers/d1/codes/chicken-pi/data/coop_20211003.fits')
+    t.write('coop_20211003.csv')
+
+    # Attach the CSV file to the page
+    confluence.attach_file('coop_20211003.csv', name='Coop Data', content_type='application/fits', page_id=page_id, comment='Test Upload')
+ 
+    # Get some information
+    info = confluence.get_attachments_from_content(page_id, start=0, limit=50)
+    print(info)
+
+
+def update_filter_characterization():
+    """update_filter_characterization Update the Confluence Page
+
+    This routine updates the Confluence page for LMI Filter Characterization
+    """
+    # Instantiate the class
+    confluence = setup_confluence()
+
+    # This the page we want to update -- we'll request the page_id for ease
+    space = 'LDTOI'
+    title = 'LMI Filter Characterization 2'
+
+    # If it doesn't already exist, create it, then get the page_id
+    if not confluence.page_exists(space, title):
+        body = "New Page!!!  You must add the appropriate macros to read the attached file."
+        confluence.create_page(space, title, body, parent_id=55214493)
+    page_id = confluence.get_page_id(space, title)
+
+    # Make sure the CSV file(s) we want to update are extant
+    filename = 'csvtable.csv'
+
+    # Remove the attachment on the Confluence page before uploading the new one
+    confluence.delete_attachment(page_id, filename)
+
+    # Attach the CSV file(s) to the Confluence page
+    confluence.attach_file(filename, name='Something', content_type='text/csv',
+                           page_id=page_id, comment='Something')
+
+
+def setup_confluence():
+    """setup_confluence Set up the Confluence class instance
+
+    Uses `keyring` to hold API credentials for Confluence, including the URL.
+
+    As such, the appropriate values for `url`, `uname`, and `passwd` need to
+    be loaded into the keyring `service` = `confluence` on the machine running
+    this code.
+
+    The python package `keyring` includes a CLI for adding this information:
+        `keyring set confluence [url,uname,passwd]`
+
+    Returns
+    -------
+    `atlassian.Confluence`
+        Confluence class, initialized with credentials
+    """
+    return Confluence( url=keyring.get_password('confluence', 'url'),
+                       username=keyring.get_password('confluence', 'uname'),
+                       password=keyring.get_password('confluence', 'passwd') )
 
 
 #=============================================================================#
@@ -22,7 +117,7 @@ def main():
     """
     This is the main body function.
     """
-    pass
+    test_confluence()
 
 
 if __name__ == "__main__":
